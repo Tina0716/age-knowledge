af2d9395895aefc95049e560bda5dfeb5b374358
Tina0716 <52436437+Tina0716@users.noreply.github.com>
2019-07-02 21:57:51 +0800

    Add files via upload

---
+++ math.py
@@ -0,0 +1,15 @@
+print("imput your age")
+
+x = int("18")
+
+y = int("12") * x
+print(y,"months")
+
+z = int("365") * x
+print(z,"days")
+
+a = int("365") * int("24") * x
+print(a,"hours")
+
+b = int("365") * int("24") * int("60") * x
+print(b,"minutes")
