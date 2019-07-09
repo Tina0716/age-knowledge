3bfa80b2cd247de474c22be975836399bdc4815d
Tina0716 <52436437+Tina0716@users.noreply.github.com>
2019-07-09 08:29:15 +0800

    Add files via upload

---
+++ 7.8.py
@@ -0,0 +1,18 @@
+import math
+x=10
+A=[4,16,25]
+B=[6,9,13]
+def sum_list(A,B):
+    for i in range(len(A)):
+        a=i.index
+    for j in range(len(B)):
+        b=j.index
+    c=a+b
+    return c
+print(sum_list(A,B))
+
+def mul(x,c):
+    for i in range(len(c)):
+        d=x*c
+    return d
+print(mul(x,c))
