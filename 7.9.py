7bbf4e0f21439bfe17f788a44072c1235dc3bc73
Tina0716 <52436437+Tina0716@users.noreply.github.com>
2019-07-10 12:36:17 +0800

    Add files via upload

--- 7.9.py
+++ 7.9.py
@@ -2,13 +2,13 @@ j_p={}
 j_n=int(input())
 j_r=int(input())
 lst=[]
-while 0<j-r<10 and 0<=j-n<100:
-    j_p[j-n]=j-r
+while 0<j_r<10 and 0<=j_n<100:
+    j_p[j_n]=j_r
     j_n=int(input())
     j_r=int(input())
 else:
     print("out of range")
 for i in j_p:
     lst.append(i)
-    lst.sort()
-print("play number is %d and the rate is %d",(b,j_p(b)))
+    b=lst.sort()
+print("play number is %d and the rate is %d"%[b,j_p(b)])
